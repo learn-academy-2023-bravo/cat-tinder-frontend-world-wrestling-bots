5e714562cd0272e0cd641d78ea1bbff46a30cb4c
borja-x13 <bxaire13@gmail.com>
2023-05-03 13:14:11 -0700

    removed the incepted app

--- wwb-frontend/src/pages/BotIndex.js
+++
@@ -1,9 +0,0 @@
-// BotIndex page structure
-
-const BotIndex = () => {
-    <>
-        <h3>All of the Bots!</h3>
-    </>
-}
-
-export default BotIndex
--- wwb-frontend/src/pages/Home.js
+++
@@ -1,9 +0,0 @@
-// homepage structure
-
-const Home = () => {
-    <>
-        <h3> Home of the Bot-o-pocalipse </h3>
-    </>
-}
-
-export default Home
